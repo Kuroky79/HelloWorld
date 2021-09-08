dabec3f5b087f8980f0790f457f468b71361f5c0
Alabuzhev daniil <alabuzhev.danil@mail.ru>
2021-09-08 17:27:18 +0300

    3 task

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(1./i);
+        }
+    }
+}
